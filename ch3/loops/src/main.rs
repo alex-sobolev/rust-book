53e352c1ac0bff6469641ec7e2593c4c27f1f7aa
Alex Sobolev <alex.sobolev1@gmail.com>
2022-01-02 18:02:10 +0100

    Add ch3 loops

---
+++ ch3/loops/src/main.rs
@@ -0,0 +1,107 @@
+fn main() {
+    // 3 types of loops:
+    // `loop`, `while`, `for`
+
+    // `loop`
+    simple_loop();
+    nested_loops();
+    return_from_loop();
+
+    // `while`
+    while_loop();
+
+    // `for`
+    for_loop();
+    for_loop_with_count();
+}
+
+fn simple_loop() {
+    let mut x = 0;
+
+    // loop is an infinite loop
+    loop {
+        if x > 9 {
+            println!("reached the limit");
+            break;
+        }
+
+        println!("again!");
+        x += 1;
+    }
+}
+
+// you can nest loops inside one another
+// and you can use labels for loops to `break` or `continue` them
+// by default if you just use `break` / `continue`: it's applied to the innermost loop
+fn nested_loops() {
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("Outer loop count: {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {}", remaining);
+
+            if remaining == 5 {
+                break;
+            }
+
+            if count == 3 {
+                break 'counting_up;
+            }
+
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+}
+
+// loops are also expressions in Rust
+// you can assign them to variables
+fn return_from_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter > 9 {
+            break counter * 10;
+        }
+    };
+
+    println!("The result: {}", result);
+}
+
+// `while` is a conditional loop
+// you achieve the same result with `loop` and `if`, `else`, `break` keywords
+// but `while` is more concise
+fn while_loop() {
+    let mut num = 9;
+
+    while num >= 0 {
+        println!("The number: {}", num);
+
+        num -= 1;
+    }
+
+    println!("Lift off!");
+}
+
+fn for_loop() {
+    let arr = [10, 20, 30, 40, 50];
+
+    for elem in arr {
+        println!("The array current number: {}", elem);
+    }
+}
+
+fn for_loop_with_count() {
+    // you can use `Range` type
+    for count in (1..4).rev() {
+        println!("{}", count);
+    }
+
+    println!("Lift off!");
+}
