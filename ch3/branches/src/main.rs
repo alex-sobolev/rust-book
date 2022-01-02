bb56311ff35a50a4fc005494bb6d0af8443e33cb
Alex Sobolev <alex.sobolev1@gmail.com>
2022-01-02 17:15:33 +0100

    Add ch3 branching logic

---
+++ ch3/branches/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let condition = true;
+
+    // `if` is an expression in Rust, so you can assign it to a variable
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+}
