1e4a1430e469fff8ff1f24523d0148344ba91725
Alex Sobolev <alex.sobolev1@gmail.com>
2022-01-21 17:58:22 +0100

    Add ch7 packages, crates, modules. Part 3

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -15,7 +15,24 @@ mod front_of_house {
 }
 
 mod back_of_house {
-    use crate::serve_order;
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 
     fn cook_order() {}
 
@@ -26,11 +43,18 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    // relative path
-    front_of_house::hosting::add_to_waitlist();
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 pub fn serve_order() {}
